85f16870337b3834cd6f0d63109c5595a42d9732
AhmedRabea0302 <arabea169@gmail.com>
2021-12-25 14:39:20 +0200

    Adding 404 Page and redirction route

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Jobs from '../views/jobs/Jobs.vue'
 import JobDetails from '../views/jobs/JobDetails.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [{
         path: '/',
@@ -19,11 +20,24 @@ const routes = [{
         name: 'Jobs',
         component: Jobs
     },
-
     {
         path: '/jobs/:id',
         name: 'JobDetails',
-        component: JobDetails
+        component: JobDetails,
+        props: true,
+    },
+
+    // Redirection
+    {
+        path: '/all-jobs',
+        redirect: '/jobs',
+    },
+
+    // Catch all 404 
+    {
+        path: '/:catchAll(.*)',
+        name: 'NotFound',
+        component: NotFound
     }
 ]
 
